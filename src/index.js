049c3603a5cd45a96755205ec75a761b74e12024
mykachitos <cccnikita2006@gmail.com>
2026-02-27 10:30:11 +0300

    обнова функций

--- src/index.js
+++ src/index.js
@@ -12,17 +12,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
-
-
-// .не нужно оборачивать App в AuthProvider дважды, достаточно одного раза. Поэтому я удалил второй вызов ReactDOM.createRoot и оставил только один, который оборачивает App в AuthProvider.
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
